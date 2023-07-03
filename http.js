6a70beac4d489c0985ccbe2e3ba80082b87a1769
Misaki <misaki@Misakis-MacBook-Air.local>
2023-07-03 15:33:17 -0700

    done

---
+++ http.js
@@ -0,0 +1,35 @@
+// What is HTTP?
+//  a: HTTP(Hypertext Transfer Protocol) is the protocol that enables the transfer of data over the internet, allowing users to access websites and other online resources.
+
+// What is a URL?
+// a : A Uniform Resource Locator  web address
+
+// What is DNS? 
+// a : The Domain Name System (DNS) turns domain names into IP addresses,
+
+// What is a query string?
+// a :A query string is a part of a uniform resource locator (URL) that assigns values to specified parameters.    
+
+// List two HTTP Verbs and their use cases.
+// a: GET: requests without side effects 
+// POST: requests with side effects
+
+
+// What is an HTTP request?
+// a : HTTP request is made by a client, to a named host, which is located on a server.
+
+// What is an HTTP response?
+// a: An HTTP response is made by a server to a client. 
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// a ;Headers provide additional information about the request or the response. 
+// Request headers: Authority,Referer,Accept
+// Response headers:Credentials,Cache-Contro,Date
+
+// What happens when you type a URL in a browser?
+// a : 1,The browser checks the cache for a DNS record to find the corresponding IP address
+// 2,browser establishes a TCP connection with the server.
+// 3,
+// The browser sends an HTTP request to the server.
+// 4,The server processes the request and sends back the response.
+// 5,he browser renders the HTML content.
